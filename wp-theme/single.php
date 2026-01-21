8c0f3f1749771ff8b8e48acfcd7772d8491d7589
Fazal Shahid Latif <64069300+FazalShahidLatif@users.noreply.github.com>
2026-01-21 23:07:33 +0500

    Create single.php file for individual post template

---
+++ wp-theme/single.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying individual posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package YourThemeName
+ */
+
+get_header();
+?>
+
+<div class="post">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <div><?php the_content(); ?></div>
+    <?php endwhile; endif; ?>
+</div>
+
+<?php
+get_footer();
+?>
