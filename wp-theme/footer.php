8e96b996884853ce4df1628a48c9208f2554bb05
Fazal Shahid Latif <64069300+FazalShahidLatif@users.noreply.github.com>
2026-01-21 21:21:31 +0500

    Added footer.php with social links and branding

---
+++ wp-theme/footer.php
@@ -0,0 +1,15 @@
+<!-- Footer Section -->
+<footer>
+    <div class="branding">
+        <h2>Taleem 360</h2>
+        <p>Your one stop solution for education.</p>
+    </div>
+    <div class="social-links">
+        <a href="https://facebook.com/FazalShahidLatif" target="_blank">Facebook</a>
+        <a href="https://twitter.com/FazalShahidLatif" target="_blank">Twitter</a>
+        <a href="https://instagram.com/FazalShahidLatif" target="_blank">Instagram</a>
+    </div>
+    <div class="copy-right">
+        <p>&copy; 2026 - All Rights Reserved.</p>
+    </div>
+</footer>
